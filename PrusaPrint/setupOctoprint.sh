62d5547eb9020a8a26ef89bbcafcc6ae129b8252
Scott Gibb <44235263+ScottGibb@users.noreply.github.com>
2023-11-17 19:13:06 +0000

    Update setupOctoprint.sh

--- PrusaPrint/setupOctoprint.sh
+++ PrusaPrint/setupOctoprint.sh
@@ -9,6 +9,8 @@ git clone https://github.com/ScottGibb/LED-Strip-Controller-Octoprint.git
 echo "Finished Download"
 
 # Install Temp Sensor drivers DHT11
+echo "Setting up DHT11 Drivers
 pip3 install adafruit-circuitpython-dht
 sudo apt-get update
 sudo apt-get install libgpiod2 -y
+echo "Setup Complete"
