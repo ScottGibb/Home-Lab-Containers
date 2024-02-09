2932e07aa9a6d1e49665322a2694f08a795c7501
Scott Gibb <Kyle.Lawson@babcockdev.onmicrosoft.com>
2024-02-09 18:49:31 +0000

    Attempting to update firefly

--- PiHome/Finances/mariadb/mySqlScript.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE fireflyiiiDb;
-create user firefly@'%' identified by 'secret_firefly_password';
-grant all privileges on fireflyiiiDb.* to firefly@'%';
-flush privileges;
-quit
--- PiHome/Finances/mariadb/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/etc/init.d/mariadb start
-mysql -u root --password="" -e "source /mysql_scripts/mySqlScript.sql"
-/etc/init.d/mariadb start && tail -f /dev/null
